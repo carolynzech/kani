c4e2090f4db6d068403674c6ab956c41be089ec0
Carolyn Zech <cmzech@amazon.com>
2024-12-31 11:04:28 -0500

    try splitting & simplifying tests to reduce runner load

--- tests/expected/function-contract/as-assertions/assert-preconditions-fail.rs
+++ tests/expected/function-contract/as-assertions/assert-preconditions-fail.rs
@@ -41,24 +41,4 @@ mod should_fail {
         let mut i = kani::any();
         add_three(&mut i);
     }
-
-    // Since add_three is the target of the proof_for_contract, its precondition gets assumed.
-    // However, add_one's precondition is still asserted, causing failure.
-    #[kani::proof_for_contract(add_three)]
-    fn prove_add_three_contract() {
-        let mut i = kani::any();
-        add_three(&mut i);
-    }
-}
-
-mod should_pass {
-    use crate::*;
-    // Same as should_fail::prove_add_one, with the added assumption of add_three's precondition.
-    // Note that add_one's precondition gets assumed since it's the target of the proof_for_contract.
-    #[kani::proof_for_contract(add_one)]
-    fn prove_add_one() {
-        let mut i = kani::any();
-        kani::assume(i < 100);
-        add_three(&mut i);
-    }
 }
